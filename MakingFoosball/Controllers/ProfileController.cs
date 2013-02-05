a5d212d04fc1393b3ac63755422f3104dc57c9bf
maridom <mariusz.domaradzki@makingwaves.pl>
2013-02-05 20:28:04 +0100

    EntityFramework project added. Connection string encrypted.

--- MakingFoosball/Controllers/ProfileController.cs
+++ MakingFoosball/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MakingFoosball.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,8 @@ namespace MakingFoosball.Controllers
     {
         public ActionResult Create()
         {
-            return View();
+            var p = new FoosballAppEntities().Users;
+            return View();            
         }
     }
 }
