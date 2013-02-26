cb010226f5a1c7595c248dfa4a42f96dcfb56adb
maridom <mariusz.domaradzki@makingwaves.pl>
2013-02-26 17:43:51 +0100

    New encryption of connectsionStrings.config.

--- DataAccess/Repositories/UsersRepository.cs
+++ DataAccess/Repositories/UsersRepository.cs
@@ -1,10 +1,6 @@
 ﻿using Common.Enums;
 using DataAccess.Repositories.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Repositories
 {
