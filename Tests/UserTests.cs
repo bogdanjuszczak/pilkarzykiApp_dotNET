6c1204bf79143f5dc04a155dad033c8af1901382
Sylwek <sylwester.jarosinski1987@gmail.com>
2013-02-26 17:58:49 +0100

    add Moq to Test project referances

---
+++ Tests/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Enums;
+using DataAccess.Repositories.Interfaces;
+
+namespace Tests
+{
+    public class UserTests
+    {
+        public void CreateUserTest()
+        {
+            var user = new Mock<IUsersRepository>();
+            user.Setup(usr => usr.CreateUser("abc", "gffad")).Returns(CreateUserStatus.DuplicatedUsername);
+
+            
+        }
+    }
+}
